5e1095cfe7213dbf58c1d06104a010bb977cee3d
Yogendra Tamang <yogen48@hotmail.com>
2020-08-22 22:46:16 +0200

    add setup update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="parse-utils",
-    version="1.3.0",
+    version="1.3.1",
     author="Yogendra Tamang",
     author_email="48yogen@gmail.com",
     description="Page Parser Utils For scraping, List index update",

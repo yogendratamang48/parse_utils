23960cfa6ec7c83f2ccbbffd1bb4e4f9ce451a84
yogendratamang48 <yogen48@hotmail.com>
2021-11-02 23:23:18 +0100

    add markdownify

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="parse-utils",
-    version="1.3.4",
+    version="1.3.6",
     author="Yogendra Tamang",
     author_email="48yogen@gmail.com",
     description="Page Parser Utils For scraping, List index update",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/yogendratamang48/parse_utils.git",
     packages=setuptools.find_packages(),
-    install_requires=["lxml"],
+    install_requires=["lxml", "markdownify"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
